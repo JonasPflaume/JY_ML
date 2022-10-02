dea355a975419c4b7ab290e7b7c647827e8d7438
Jiayun <lijiayun2016@gmail.com>
2022-10-02 23:46:50 +0200

    add the benchmark

--- jylearn/data/reg_data.py
+++ jylearn/data/reg_data.py
@@ -1,5 +1,9 @@
 import numpy as np
+from scipy.io import loadmat
+import os
+path = os.path.dirname(__file__)
 np.random.seed(6)
+
 def polynomial_data(p=12, scale=3.5):
     X = np.linspace(-1.2,1.2,200)
     weight = np.random.uniform([-15]*p,[15]*p)
@@ -9,9 +13,13 @@ def polynomial_data(p=12, scale=3.5):
     Y += np.random.normal(scale=scale, size=[200])
     return X.reshape(-1,1), Y.reshape(-1,1)
     
+def robot_inv_data():
+    train_data = loadmat(os.path.join(path, 'sarcos_inv.mat'))['sarcos_inv'].astype(np.float32)
+    val_data, train_data = train_data[:4448], train_data[4484:].astype(np.float32)
+    test_data = loadmat(os.path.join(path, 'sarcos_inv_test.mat'))['sarcos_inv_test'].astype(np.float32)
+
+    X_train, Y_train = train_data[:, :21], train_data[:, 21:]
+    X_val, Y_val = val_data[:, :21], val_data[:, 21:]
+    X_test, Y_test = test_data[:, :21], test_data[:, 21:]
     
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    X, Y = polynomial_data()
-    plt.plot(X, Y, '.r')
-    plt.show()
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
--- jylearn/linear/rg.py
+++ jylearn/linear/rg.py
@@ -1,16 +1,17 @@
 import torch
-from regression import Regression
+from jylearn.linear.regression import Regression
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 class RidgeReg(Regression):
     ''' ridge regression, hyperparameter determined by cross validation
     '''
     def __init__(self) -> None:
         super().__init__()
-        self.labd = torch.logspace(-7,7,14)
+        self.labd = torch.logspace(-4,5,9)
         
-    def fit(self, X, Y, K=6, plot=False):
+    def fit(self, X, Y, K=5, plot=False):
         ''' use K-fold cross validation to get the penality
             X: (N, feature)
             Y: (N, output)
@@ -19,7 +20,7 @@ class RidgeReg(Regression):
         K_index = torch.randperm(len(X))
         K_len = len(X) // K
         loss_mean, loss_var = [], []
-        for labd in self.labd:
+        for labd in tqdm( self.labd ):
             loss_K = []
             for k in range(K):
                 val_index = K_index[k*K_len:(k+1)*K_len]
@@ -57,30 +58,3 @@ class RidgeReg(Regression):
     
     def predict(self, x):
         return x @ self.weight
-    
-if __name__ == "__main__":
-    from jylearn.data.reg_data import polynomial_data
-    import matplotlib.pyplot as plt
-    from sklearn.preprocessing import PolynomialFeatures
-    #### data
-    X, Y = polynomial_data()
-    for order in range(1, 20):
-        
-        poly = PolynomialFeatures(order)
-        X_poly = poly.fit_transform(X)
-        #### training and testing
-        X_poly, Y_ = torch.from_numpy(X_poly), torch.from_numpy(Y)
-        
-        X_test = np.linspace(np.min(X), np.max(X), 50).reshape(-1,1)
-        X_test_poly = poly.fit_transform(X_test)
-
-        X_test_poly = torch.from_numpy(X_test_poly)
-        rr = RidgeReg().fit(X_poly, Y_, plot=True)
-        pred = rr.predict(X_test_poly)
-        
-        beta = torch.inverse(X_poly.T @ X_poly) @ X_poly.T @ Y
-        pred_ls = X_test_poly @ beta
-        plt.plot(X.squeeze(), Y.squeeze(), '.r')
-        plt.plot(X_test.squeeze(), pred.squeeze(), '-b')
-        plt.plot(X_test.squeeze(), pred_ls.squeeze(), '-c')
-        plt.show()
