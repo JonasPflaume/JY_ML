373d4fa6b4bff077a553464f6fa36bcf272dd7a7
Jiayun <lijiayun2016@gmail.com>
2022-10-28 15:49:46 +0200

    finish the parameters class

--- jylearn/kernel/kernels.py
+++ jylearn/kernel/kernels.py
@@ -1,33 +1,98 @@
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 import torch as th
 import torch.nn as nn
+from enum import Enum
+
+def next_n_alpha(s, next_n):
+    return chr((ord(s.upper()) + next_n - 65) % 26 + 65)
+
+class KernelOperation(Enum):
+    EXP = "exp"
+    ADD = "add"
+    MUL = "mul"
 
 class Parameters(
-                    namedtuple('ParametersInfo', 
-                    ("name", "dim", "tensor", "bounds"))
+                namedtuple('ParametersInfo', 
+                ("tensor_dict", "dim_list", "operation_dict"))
                 ):
     
     __slots__ = ()
     
-    def __new__(cls, name, dim, tensor, bounds):
-        
+    def __new__(cls, name, dim, tensor, requres_grad=True):
+        ''' we use number to indicate the operation sequence
+        '''
         if not isinstance(name, str):
             raise TypeError("Initialize the parameters with a string of name corresponding to the kernel.")
         if not isinstance(dim, int):
             raise TypeError("The dimension of parameters should be a integer.")
         if not isinstance(tensor, nn.parameter.Parameter):
             raise TypeError("We use pytorch to optimize hyperparameters.")
-        if not isinstance(bounds, th.Tensor):
-            raise TypeError("We use pytorch to optimize hyperparameters.")
         
         assert len(tensor) == dim, "false dimension or tensor."
-        assert len(bounds.shape) == 2, "second dimension for upper and lower bounds."
-        assert bounds.shape[0] == dim, "align the dimension please."
-        assert bounds.shape[1] == 2, "Lower and upper bounds."
         
-        return super(Parameters, cls).__new__(cls, name, dim, tensor, bounds)
+        tensor.requires_grad = requres_grad
         
+        tensor_dict = OrderedDict()
+        tensor_dict[name] = tensor
+        
+        operation_dict = OrderedDict()
+        operation_dict['A'] = name
+        return super(Parameters, cls).__new__(cls, tensor_dict, [dim], operation_dict)
+    
+    def join(self, param2, operation:KernelOperation):
+        ''' join two parameters instance
+            parameters will be concatenated as a set.
+            The operator will be recoreded as a dict by steps.
+        '''
+        if not isinstance(operation, KernelOperation):
+            raise TypeError("Use the default operation please!")
+        
+        param2_tensor_dict_keys = list( param2.tensor_dict.keys())
+
+        if operation == KernelOperation.EXP:
+            assert len(param2_tensor_dict_keys) == 1
+            assert param2.tensor_dict[param2_tensor_dict_keys[0]].shape == (1,), "The exponentiation needs a scalar."
+            assert param2.tensor_dict[param2_tensor_dict_keys[0]].requires_grad == False, \
+                "The exponentiation should be a fixed parameter. Please set the requires_grad of Parameters to False."
+            
+        # append the operation chain from param2 to self
+        max_operation_self_ori = max(self.operation_dict.keys())
+        increase_n = ord(max_operation_self_ori) - 65 + 1
+ 
+        # lift the numbering of operation dict in param2
+        temp_dict = OrderedDict()
+        
+        for key in param2.operation_dict.keys():
+            transformed_operation = [next_n_alpha(character, increase_n) if character.isupper() else character for character in param2.operation_dict[key]]
+            temp_dict[next_n_alpha(key, increase_n)] =  "".join(transformed_operation)
+
+        # concatenate operation dict
+        self.operation_dict.update(temp_dict)
+
+        # update the parameters lists
+        self.tensor_dict.update(param2.tensor_dict)
+        
+        max_operation_self = max(self.operation_dict.keys())
+        # update operation dict
+        self.operation_dict[next_n_alpha(max_operation_self, 1)] = " ".join([
+            max_operation_self_ori,
+            operation.value,
+            max_operation_self
+        ])
+        
+    def __repr__(self) -> str:
+        info =  "".join(["%"]*100) + "\n"\
+                "This is a parameters group composed by: " +\
+                ",".join(self.tensor_dict.keys()) + "\n" + \
+                "by operation numbering: \t" + ",".join(self.operation_dict.keys()) + "\n" + \
+                "operations are \t" + ",".join(self.operation_dict.values()) + "\n" +\
+                "".join(["%"]*100)
+        return info
+
 class Kernel(nn.Module):
+    
+    def register_parameters(self, parameters_cls):
+        pass
     
     def forward(self, x):
         pass
@@ -50,33 +115,42 @@ class Kernel(nn.Module):
     def __repr__(self):
         pass
     
-    def _check_bounds(self):
-        pass
-    
-class Sum(nn.modules):
-    pass
+class Sum(Kernel):
+    '''
+    '''
 
-class Product(nn.modules):
-    pass
+class Product(Kernel):
+    '''
+    '''
 
-class Exponentiation(nn.Module):
-    pass
+class Exponentiation(Kernel):
+    '''
+    '''
 
+#####################
+##      Kernels    ##
+#####################
 
 class RBF(Kernel):
-    pass
+    '''
+    '''
 
 class Matern(Kernel):
-    pass
+    '''
+    '''
 
 class RQK(Kernel):
-    pass
+    '''
+    '''
 
 class Period(Kernel):
-    pass
+    '''
+    '''
 
 class Constant(Kernel):
-    pass
+    '''
+    '''
 
-class InnerProduct(Kernel):
-    pass
+class DotProduct(Kernel):
+    '''
+    '''
---
+++ jylearn/kernel/test.py
@@ -0,0 +1,23 @@
+import torch as th
+import torch.nn as nn
+from kernels import Parameters
+from kernels import KernelOperation
+
+### Test the parameter group
+t = th.zeros(3,)
+P1 = Parameters("1a", 3, nn.parameter.Parameter(t))
+
+P2 = Parameters("2a", 3, nn.parameter.Parameter(t))
+
+P1.join(P2, KernelOperation.ADD)
+
+t = th.zeros(3,)
+P2 = Parameters("3a", 3, nn.parameter.Parameter(t))
+P2.join(P1, KernelOperation.ADD)
+
+t = th.zeros(1,)
+P3 = Parameters("4a", 1, nn.parameter.Parameter(t), requres_grad=False)
+P2.join(P3, KernelOperation.EXP)
+print(P2)
+
+### Seems OK
